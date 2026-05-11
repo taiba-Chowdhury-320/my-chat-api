11108704e01548cb9710a862d9482c48700192d8
taiba-Chowdhury-320 <facebookbotidrocky320@gmail.com>
2026-05-11 19:10:04 +0600

    Update Teacher.js

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,1 +1,8 @@
+import mongoose from "mongoose";
 
+const TeacherSchema = new mongoose.Schema({
+  userID: { type: String, required: true, unique: true },
+  count: { type: Number, default: 0 },
+}, { timestamps: true });
+
+export default mongoose.models.Teacher || mongoose.model("Teacher", TeacherSchema);
